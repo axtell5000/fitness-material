f0e03ac6f7767c46d9669a79fcc99f602a433714
Stephen Axtell <axtell.stephen@gmail.com>
2018-07-28 20:04:14 +0200

    split the apps modules by features

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '../material.module';
+
+// Even though its in the App Module. Modules dont filter down even to their children
+// That why you may have to repeat some imports. Although this doesnt increase size of App at compilation
+
+
+@NgModule({
+  declarations: [
+    SignupComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule
+  ]
+})
+export class AuthModule {}
---
+++ src/app/training/training.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
+
+import { TrainingComponent } from './training.component';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
+import { MaterialModule } from '../material.module';
+import { StopTrainingComponent } from './current-training/stop-training.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    StopTrainingComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AngularFirestoreModule,
+    FormsModule
+  ],
+  entryComponents: [StopTrainingComponent] //
+})
+
+export class TrainingModule {}
