a19e63701bb7243a8f1a416e305258a701afd79a
Stephen Axtell <axtell.stephen@gmail.com>
2018-07-29 16:20:52 +0200

    Split routing into specific modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 // Even though its in the App Module. Modules dont filter down even to their children
 // That why you may have to repeat some imports. Although this doesnt increase size of App at compilation
@@ -18,7 +19,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     ReactiveFormsModule,
     AngularFireAuthModule,
-    SharedModule
+    SharedModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule {}
